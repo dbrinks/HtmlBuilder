f12ef7cb979d9c45922f8a6d97669a9afde97607
Devon Brinks <brinks.d@gmail.com>
2013-02-08 15:10:03 -0500

    Basic files and a bit of work

---
+++ src/HtmlBuilder.Tests/HtmlBuilderTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HtmlBuilder.Tests
+{
+
+    [TestClass]
+    public class HtmlBuilderTests
+    {
+        [TestMethod]
+        public void HtmlBuilder_NoSelectorPassed_RendersEmptyString()
+        {
+            var html = new HtmlBuilder();
+
+            Assert.AreEqual(string.Empty, html.ToString());
+        }
+
+        [TestMethod]
+        public void HtmlBuilder_TagNamePassed_RendersTag()
+        {
+            var html = new HtmlBuilder("div");
+
+            Assert.AreEqual("<div></div>", html.ToString());
+        }
+    }
+}
---
+++ src/HtmlBuilder/HtmlBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace HtmlBuilder
+{
+    public class HtmlBuilder
+    {
+        public string Selector { get; set; }
+        public List<HtmlBuilder> InnerHtml { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public HtmlBuilder()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="selector"></param>
+        public HtmlBuilder(string selector)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+
+    public class SelectorParser
+    {
+
+    }
+}
